cd05221af6760027c25a585dc98e4c5f890aa2c4
VIVIN KRISHNAN <vivinkrishnan@in.ibm.com>
2016-03-13 20:26:25 +0530

    ResourceRequestWin10  Sample

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# ResourceRequestWin10
+IBM MobileFirst Platform Foundation
+===
+## ResourceRequestWin10
+A sample application demonstrating the use to the WorklightResourceRequest API in C#.
+
+### Tutorials
+https://mobilefirstplatform.ibmcloud.com/tutorials/en/foundation/8.0/using-the-mfpf-sdk/resource-request-from-windows-8-10-applications/
+
+### Usage
+
+1. From the command line, navigate to the project's root folder.
+2. Ensure the sample is registered in the MobileFirst Server by running the command: `mfpdev app register`.
+3. The sample uses the `JavaAdapter` contained in the Adapters Maven project. Use either Maven or MobileFirst Developer CLI to [build and deploy the adapter](https://mobilefirstplatform.ibmcloud.com/tutorials/en/foundation/8.0/adapters/creating-adapters/).
+4. To test or debug an adapter, see the [testing and debugging adapters](https://mobilefirstplatform.ibmcloud.com/tutorials/en/foundation/8.0/adapters/testing-and-debugging-adapters) tutorial.
+5. Import the project to Android Studio, and run the sample by clicking the **Run** button.
+
+Resource Request adapter: https://github.com/MobileFirst-Platform-Developer-Center/Adapters/tree/release80
+
+### Supported Levels
+IBM MobileFirst Platform Foundation 8.0
+
+### License
+Copyright 2016 IBM Corp.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ ResourceRequestWin10/MainPage.xaml.cs
@@ -0,0 +1,99 @@
+﻿/**
+* Copyright 2016 IBM Corp.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Diagnostics;
+using Worklight;
+using System.Text;
+using Windows.UI;
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ResourceRequestWin10
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Submit clicked");
+            try
+            {
+                IWorklightClient _newClient = WorklightClient.CreateInstance();
+
+                StringBuilder uriBuilder = new StringBuilder().Append("/adapters").Append("/JavaAdapter").Append("/users")
+                                                .Append("/").Append(this.firstname.Text)
+                                                .Append("/").Append(this.middlename.Text)
+                                                .Append("/").Append(this.lastname.Text);
+                
+                Debug.WriteLine(new Uri(uriBuilder.ToString(), UriKind.Relative));
+
+                WorklightResourceRequest rr = _newClient.ResourceRequest(new Uri(uriBuilder.ToString(), UriKind.Relative), "POST", "");
+
+                rr.SetQueryParameter("age", this.age.Text);
+
+                KeyValuePair<String, String> headers = new KeyValuePair<string, string>("birthdate", this.date.Text);
+
+                rr.SetHeader(headers);
+
+                Dictionary<string, string> formParams = new Dictionary<string, string>();
+                formParams.Add("height", this.date.Text);
+
+                WorklightResponse resp = await rr.Send(formParams);
+
+                Debug.WriteLine(">>>>> After rr.send()");
+
+                System.Diagnostics.Debug.WriteLine(resp.ResponseText);
+
+                System.Diagnostics.Debug.WriteLine(resp.ResponseJSON);
+
+                this.Console.Text = resp.ResponseText;
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.StackTrace);
+            }
+
+        }
+
+        private void ClearConsole(object sender, DoubleTappedRoutedEventArgs e)
+        {
+            Console.Text = "";
+        }
+
+    }
+}
